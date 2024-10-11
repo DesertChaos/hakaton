e5d62cf9b7d43f2be038a6534b7f40269d245ee9
DesertChaos <vadimsysolin52565@gmail.com>
2024-10-11 23:15:50 +0300

    third commit

--- bot.py
+++ bot.py
@@ -1,24 +1,20 @@
 import telebot
 from telebot import types
 from ultralytics import YOLO
-import time
- 
+
 # Загрузить модель
-model = YOLO(r"C:\Users\Вадим\Desktop\tg_bot\best.pt")  # pretrained YOLO11n model
+model = YOLO("yolo11n.pt") # pretrained YOLO11n model
 
 API_TOKEN = '7602626066:AAHKb0BWmiPral734oHGJeNWDDK4JBqlKjo'
-bot = telebot.TeleBot(API_TOKEN)  # Ключ
-
-# Установка вебхука
-bot.delete_webhook()
-bot.set_webhook(url='https://lemon-chefs-rule.loca.lt/bot' + API_TOKEN)
+bot = telebot.TeleBot(API_TOKEN) # Ключ
 
 def detect_objects(image_path):
   # Запуск инференса на изображении
-  results = model([image_path])  # Вернет список объектов результатов
+  results = model([image_path]) # Вернет список объектов результатов
 
   # Обработка результатов
-  result = results[0]  # берем первый (и в данном случае единственный) результат
+  result = results[0] # берем первый (и в данном случае единственный) результат
+  result.show() # отображение на экране (для отладки, если нужно)
 
   # Путь для сохранения аннотированного изображения
   annotated_image_path = "annotated_image.jpg"
@@ -42,13 +38,23 @@ def get_text_message(message):
   elif message.text == 'Загрузить фото':
     bot.send_message(message.chat.id, 'Пожалуйста, отправьте фото ноутбука для анализа.')
   elif message.text == 'Информация':
-    bot.send_message(message.chat.id, 'Привет, я бот который найдет поломку в твоем ноутбуке. Чтобы я нашел ошибку загрузи картинку и при помощи нейросетей я выдам тебе результат с возможными поломками.')
+    bot.send_message(message.chat.id, 'Привет, я бот который найдет поломку в твоем ноутбуке. Чтобы я нашел ошибку загрузи картинку/видео и при помощи нейросетей я выдам тебе результат с возможными поломками.')
   elif message.text == 'Тех. поддержка':
     bot.send_message(message.chat.id, '[Администратор](https://t.me/hantik_X)', parse_mode='Markdown')
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
-  bot.send_message(message.chat.id, 'Фото получено! Обрабатываем...')
+  file_info = bot.get_file(message.photo[-1].file_id)
+  downloaded_file = bot.download_file(file_info.file_path)
 
-while True:
-    time.sleep(1)
+  photo_path = "received_photo.jpg"
+  with open(photo_path, 'wb') as new_file:
+    new_file.write(downloaded_file)
+
+  annotated_image_path = detect_objects(photo_path)
+
+  with open(annotated_image_path, 'rb') as img_file:
+    bot.send_photo(message.chat.id, img_file)
+
+if __name__ == "__main__":
+  bot.polling(none_stop=True)
